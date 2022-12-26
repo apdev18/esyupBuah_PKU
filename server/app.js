ad2bfd6a3e03632a5f32479e382ed62581272ab1
Alga Prananda <apdevs@icloud.com>
2022-12-26 19:20:48 +0700

    push to github

---
+++ server/app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
+
+// Import Router
+const authRouter = require("./routes/auth");
+const categoryRouter = require("./routes/categories");
+const productRouter = require("./routes/products");
+const brainTreeRouter = require("./routes/braintree");
+const orderRouter = require("./routes/orders");
+const usersRouter = require("./routes/users");
+const customizeRouter = require("./routes/customize");
+
+// Import Auth middleware for check user login or not~
+const { loginCheck } = require("./middleware/auth");
+const CreateAllFolder = require("./config/uploadFolderCreateScript");
+
+/* Create All Uploads Folder if not exists | For Uploading Images */
+CreateAllFolder();
+
+// Database Connection
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true });
+        console.log(`Database Connected Successfully: ${conn.connection.host}`);
+      } catch (err) {
+        console.log("Database Not Connected");
+      }      
+}
+
+// Middleware
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+app.use("/api", authRouter);
+app.use("/api/user", usersRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/product", productRouter);
+app.use("/api", brainTreeRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/customize", customizeRouter);
+
+// Test Build for Cyclic
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"),
+  function(err){
+    res.status(500).send(err);
+  });
+});
+
+// Run Server
+const PORT = process.env.PORT;
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on ", PORT);
+  })
+})
